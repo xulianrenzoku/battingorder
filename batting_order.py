ac46bbff775046b0e61cb7f425d4a80f7d4d2ce5
Xu Lian <xlian2@usfca.edu>
2019-07-22 14:00:47 -0700

    initial add

---
+++ batting_order.py
@@ -0,0 +1,172 @@
+import requests
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+from time import sleep
+
+
+def fetch(url):
+    """
+    Use BeautifulSoup to get the text of a web page.
+    """
+    r = requests.get(url)
+    comm = re.compile("<!--|-->")
+    soup = BeautifulSoup(comm.sub("", r.text), "html.parser")
+    return soup
+
+
+def get_team_page(season_page_url):
+    """
+    This function is to get the url's of all 30 MLB teams in a given year.
+    """
+    soup = fetch(season_page_url)
+    url_list = []
+    for item in soup.find_all('th',
+                              {'scope':"row",
+                               'class':"left ",
+                               'data-stat':"team_ID"}
+                              ):
+        if item.a:
+            url = "https://www.baseball-reference.com" + item.a['href']
+            url_list.append(url)
+    return list(set(url_list))
+
+
+def get_game_page(team_page_url):
+    """
+    This function is to get all the url's of a single team's record
+    in a given year.
+    The input is a single team's season page.
+    """
+    soup = fetch(team_page_url)  # Call fetch()
+    url_list = []
+    for item in soup.find_all('span', {'class':"poptip"}):
+        winTag = item.find(class_= "count win")
+        lossTag = item.find(class_= "count notwin")
+        if winTag:
+            url = "https://www.baseball-reference.com" + item.a['href']
+            url_list.append(url)
+        elif lossTag:
+            url = "https://www.baseball-reference.com" + item.a['href']
+            url_list.append(url)
+    return url_list
+
+
+def get_play_by_play(game_page_url):
+    """
+    This function is to get the raw html table regarding play-by-play
+    given a box score page.
+    The input is a single game's page.
+    """
+    soup = fetch(game_page_url)
+    target_table = soup.findAll('table', id="play_by_play")
+    return target_table[0]
+
+
+def html2pd(pbp_table):
+    """
+    This function is to convert a html play-by-play table to a pandas dataframe.
+    The input is a signle game's html table.
+    """
+    column_flag = 1
+    column_list = []
+    pbp_list = []
+    for row in pbp_table.find_all('tr'):
+        # Get column names
+        if column_flag == 1:
+            for item in row.find_all('th'):
+                column_list.append(item.text)
+            column_flag = 0
+        # Get row records
+        else:
+            row_list = []
+            for item in row.find_all('th'):
+                row_list.append(item.text)
+            for item in row.find_all('td'):
+                row_list.append(item.text)
+            pbp_list.append(row_list)
+    # Create pandas dataframe
+    pbp_df = pd.DataFrame(columns=column_list)
+    pbp_list_adj = [row for row in pbp_list if len(row) == len(column_list)]
+    for i in range(len(pbp_list_adj)):
+        pbp_df.loc[i] = pbp_list_adj[i]
+    return pbp_df
+
+
+def get_pa(game_page_url, team):
+    """
+    This function is to get the desired info from the play-by-play table.
+    The input is a single game's page and a team's name.
+    """
+    pbp_table = get_play_by_play(game_page_url)  # Get HTML
+    pbp_df = html2pd(pbp_table)  # Convert HTML to pandas df
+    team_df = pbp_df[pbp_df["@Bat"] == team]  # Only focus on the desired team
+    # Following chunk tries to eliminate non-plate-appearance actions
+    batter_list = list(team_df.Batter)
+    pa_list = []
+    for i in range(len(batter_list)):
+        if i == 0:
+            pa_list.append(1)
+        else:
+            if batter_list[i] != batter_list[i - 1]:
+                pa_list.append(1)
+            else:
+                pa_list[i - 1] = 0
+                pa_list.append(1)
+    # Check if the last play is incurred by the runner on bases
+    last_play = team_df["Play Description"].values[-1].lower()
+    if last_play == "picked off" or last_play == "caught stealing":
+        pa_list[-1] = 0
+    team_df["PA"] = pa_list  # Add column PA to df
+    # Filter out non-PA-related actions
+    team_df_PA = team_df[team_df["PA"] == 1]
+    # Add a new column showing whether there is a runner on base
+    team_df_PA["is_OB"] = team_df_PA.RoB.apply(
+        lambda x: 0 if x == '---' else 1)
+    # Add a new column showing whether there is a runner on socring position
+    team_df_PA["is_ISP"] = team_df_PA.RoB.apply(
+        lambda x: 1 if '2' in x or '3' in x else 0)
+    # Add batting order (1-9)
+    team_df_PA["Batting_Order"] = [i % 9 + 1 for i in range(len(team_df_PA))]
+    # Get the sum of PA and other stats for each batting position
+    team_df_gb = team_df_PA.groupby(by='Batting_Order')[
+        ["PA", "is_OB", "is_ISP"]].sum().reset_index()
+    team_df_gb["Team"] = team  # Add team name
+    team_df_gb["URL"] = game_page_url
+    return team_df_gb
+
+
+def get_team_pa(team_page_url):
+    """
+    This function is to get the desired info for all 162 games of one team.
+    The input is a single teams' page for a single season.
+    """
+    team = team_page_url.split("/")[-2]  # Get team name
+    print(f"Team: {team}")  # Print team name
+    game_pages = get_game_page(team_page_url)  # Get game url
+    team_df_list = []
+    count = 0
+    for game_page in game_pages:
+        # sleep(1)  # 1 second delay b/w scraping request
+        # Print progress
+        count += 1
+        if count % 30 == 0:
+            print(f"{count} Games Done")
+        team_df = get_pa(game_page, team)  # Get df for a single game
+        team_df["GM"] = count  # Add game number
+        team_df_list.append(team_df)
+    print(f"{len(team_df_list)} Games in Total\n")  # Print total games played
+    return pd.concat(team_df_list)
+
+
+def get_season_pa(season_page_url):
+    """
+    This function is to get the desired info of all the teams for one season.
+    The input is a single season' page.
+    """
+    year = season_page_url.split('/')[-1].split('.')[0]  # Get year
+    print(f"Season: {year}\n")
+    team_pages = get_team_page(season_page_url)
+    season_df = pd.concat([get_team_pa(team_page) for team_page in team_pages])
+    season_df['Season'] = year
+    return season_df
