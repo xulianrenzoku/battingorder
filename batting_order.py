0b81f56af689db7739353ccfa9ef0ebc2738614a
Xu Lian <xlian2@usfca.edu>
2019-07-29 12:18:54 -0700

    Add validation function

--- batting_order.py
+++ batting_order.py
@@ -28,7 +28,8 @@ def get_team_page(season_page_url):
                               ):
         if item.a:
             url = "https://www.baseball-reference.com" + item.a['href']
-            url_list.append(url)
+            title = item.a['title']
+            url_list.append((url, title))
     return list(set(url_list))
 
 
@@ -52,7 +53,7 @@ def get_game_page(team_page_url):
     return url_list
 
 
-def get_play_by_play(game_page_url):
+def get_info(game_page_url, team_page_title):
     """
     This function is to get the raw html table regarding play-by-play
     given a box score page.
@@ -60,13 +61,15 @@ def get_play_by_play(game_page_url):
     """
     soup = fetch(game_page_url)
     target_table = soup.findAll('table', id="play_by_play")
-    return target_table[0]
+    team_title_adj = team_page_title.replace(" ", "") + 'batting'
+    target_table_val = soup.findAll('table', id=team_title_adj)
+    return target_table[0], target_table_val[0]
 
 
 def html2pd(pbp_table):
     """
     This function is to convert a html play-by-play table to a pandas dataframe.
-    The input is a signle game's html table.
+    The input is a single game's html play-by-play table.
     """
     column_flag = 1
     column_list = []
@@ -93,13 +96,30 @@ def html2pd(pbp_table):
     return pbp_df
 
 
-def get_pa(game_page_url, team):
+def get_val_pa(pa_table):
+    """
+    This function is to get the actual plate appearances for a team in a game.
+    The input is a single game's html batting table.
+    """
+    team_row_list = []
+    for row in pa_table.find_all('tr'):
+        for item in row.find_all('th'):
+            # Find row that contains team total stats
+            if item.text == 'Team Totals':
+                for item in row.find_all('td'):
+                    team_row_list.append(item.text)
+    # index 6 is the total plate appearance for a team in a game
+    return int(team_row_list[6])
+
+
+def get_pa(game_page_url, team, team_page_title):
     """
     This function is to get the desired info from the play-by-play table.
     The input is a single game's page and a team's name.
     """
-    pbp_table = get_play_by_play(game_page_url)  # Get HTML
+    pbp_table, pa_table = get_info(game_page_url, team_page_title)  # Get HTML
     pbp_df = html2pd(pbp_table)  # Convert HTML to pandas df
+    team_actual_pa = get_val_pa(pa_table)  # Get a team's actual PA
     team_df = pbp_df[pbp_df["@Bat"] == team]  # Only focus on the desired team
     # Following chunk tries to eliminate non-plate-appearance actions
     batter_list = list(team_df.Batter)
@@ -113,13 +133,20 @@ def get_pa(game_page_url, team):
             else:
                 pa_list[i - 1] = 0
                 pa_list.append(1)
-    # Check if the last play is incurred by the runner on bases
-    last_play = team_df["Play Description"].values[-1].lower()
-    if last_play == "picked off" or last_play == "caught stealing":
-        pa_list[-1] = 0
     team_df["PA"] = pa_list  # Add column PA to df
     # Filter out non-PA-related actions
-    team_df_PA = team_df[team_df["PA"] == 1]
+    team_df_PA = team_df[team_df["PA"] == 1].reset_index()\
+                                            .drop("index", axis=1)
+    # Filter out extreme cases
+    # Check if the last play of each inning is incurred by the runner on bases
+    for i in range(len(team_df_PA)):
+        if team_df_PA.loc[i].Out == '2':
+            p_des = team_df_PA.loc[i]['Play Description'].lower()
+            if 'picked off' in p_des or 'caught stealing' in p_des:
+                if 'struck out' not in p_des:
+                    team_df_PA.loc[i] = 0
+    team_df_PA = team_df_PA[team_df_PA["PA"] == 1].reset_index()\
+                                                  .drop("index", axis=1)
     # Add a new column showing whether there is a runner on base
     team_df_PA["is_OB"] = team_df_PA.RoB.apply(
         lambda x: 0 if x == '---' else 1)
@@ -133,14 +160,20 @@ def get_pa(game_page_url, team):
         ["PA", "is_OB", "is_ISP"]].sum().reset_index()
     team_df_gb["Team"] = team  # Add team name
     team_df_gb["URL"] = game_page_url
+    # Validation
+    if team_df_gb["PA"].sum() != team_actual_pa:
+        print(game_page_url)
     return team_df_gb
 
 
-def get_team_pa(team_page_url):
+def get_team_pa(team_page):
     """
     This function is to get the desired info for all 162 games of one team.
-    The input is a single teams' page for a single season.
+    The input is a set.
+    Index 0 is the team page url and index 1 is the team page title.
     """
+    team_page_url = team_page[0]
+    team_page_title = team_page[1]
     team = team_page_url.split("/")[-2]  # Get team name
     print(f"Team: {team}")  # Print team name
     game_pages = get_game_page(team_page_url)  # Get game url
@@ -152,7 +185,8 @@ def get_team_pa(team_page_url):
         count += 1
         if count % 30 == 0:
             print(f"{count} Games Done")
-        team_df = get_pa(game_page, team)  # Get df for a single game
+        # Get df for a single game
+        team_df = get_pa(game_page, team, team_page_title)
         team_df["GM"] = count  # Add game number
         team_df_list.append(team_df)
     print(f"{len(team_df_list)} Games in Total\n")  # Print total games played
